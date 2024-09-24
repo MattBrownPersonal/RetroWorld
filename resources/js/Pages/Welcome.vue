7476492552b11a5d9b495eb33125d2b322ed33a5
Matt <tonganninja@gmail.com>
2024-09-24 15:46:34 +0100

    Initial Commit with welcome page

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { Head, Link } from "@inertiajs/vue3";
+import Header from "./components/HeaderComponent.vue";
+
+defineProps<{
+    canLogin?: boolean;
+    canRegister?: boolean;
+    laravelVersion: string;
+    phpVersion: string;
+}>();
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <div class="bg-gray-50 dark:bg-zinc-800 dark:teal-400">
+        <div class="relative min-h-screen">
+            <div class="w-full">
+                <main class="pt-2">
+                    <div class="flex flex-col mx-36 space-y-4 h-screen">
+                        <div class="grid grid-cols-12">
+                            <Header />
+                        </div>
+                        <div
+                            class="text-center border-2 dark:border-teal-600 bg-teal-200 py-2 dark:text-zinc-809 text-xl"
+                        >
+                            a customer message goes here!
+                        </div>
+                        <!-- <div
+                            class="dark:bg-teal-200 py-2 grid grid-cols-12 pl-5 text-center"
+                        >
+                            <div>Home</div>
+                            <div>Peripherals</div>
+                            <div>Figures</div>
+                            <div>Guides</div>
+                            <div>Merchandise</div>
+                            <div>About Us</div>
+                        </div> -->
+                        <div class="grid grid-cols-2 gap-4 py-2 w-full h-full text-center text-5xl font-VT323">
+                            <div class="main-square dark:bg-blue-500 dark:border-blue-900 shadow-2xl shadow-blue-900">
+                                Welcome to Retro World
+                            </div>
+                            <div class="main-square dark:bg-red-500 dark:border-red-900 shadow-2xl shadow-red-900">
+                                We buy and sell retro games
+                            </div>
+                            <div class="main-square dark:bg-green-500 dark:border-green-900 shadow-2xl shadow-green-900">
+                                Click here to Check out our selection of retro gaming and accessories!
+                            </div>
+                            <div class="main-square dark:bg-yellow-500 dark:border-yellow-900 shadow-2xl shadow-yellow-900">
+                                Retro World is open Mon to Sat at 20 Osmaston Road, Derby, DE12HR
+                            </div>
+                            <Link href="" class="main-square col-span-2 dark:border-teal-600 bg-teal-200">
+                                <div >Enter Our Shop!</div>
+                            </Link>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/components/HeaderComponent.vue
@@ -0,0 +1,59 @@
+<template>
+        <div class="font-horizon text-6xl dark:text-teal-400 col-span-11">
+            RETROWORLD
+            <div class="font-Montserrat text-lg flex items-center">
+                <svg class="h-8 w-8 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                </svg>
+                <a href="tel:+01332200083">01332200083</a>
+                <svg class="h-8 w-8" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <rect x="5" y="2" width="14" height="20" rx="2" ry="2" />
+                    <line x1="12" y1="18" x2="12.01" y2="18" />
+                </svg>
+                <a href="tel:+447305086858">0447305086858</a>
+            </div>
+
+        </div>
+        <div class="grid grid-flow-col gap-2 pt-8">
+            <a href="https://twitter.com/RETROWORLDS">
+                <span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-black">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512">
+                        <path
+                            d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+                    </svg>
+                </span>
+            </a>
+            <a href="https://www.facebook.com/OnlineRetroGames/">
+                <span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#1877f2]">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <path
+                            d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+                    </svg>
+
+                </span>
+            </a>
+            <a href="https://instagram.com/derbyretroworld/">
+                <span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#c13584]">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path
+                            d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+                    </svg>
+                </span>
+            </a>
+            <a href="https://www.youtube.com/channel/UCi6_s91N56YtmcTmKezCRew">
+                <span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#ff0000]">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                        <path
+                            d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" />
+                    </svg>
+                </span>
+            </a>
+        </div>
+
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
